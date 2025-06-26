8aa52b4a0cff9a306917facc5dde493022b5bd47
Jay Shirgupe <96529359+Jay7221@users.noreply.github.com>
2025-06-26 22:15:04 +0530

    Update keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,1 +1,12 @@
+vim.g.mapleader = " "
 
+local keymap = vim.keymap -- for conciseness
+
+keymap.set("i", "jk", "<ESC>")
+
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
+keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>")
+keymap.set("n", "<leader>fb", ":Telescope buffers<CR>")
+keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>")
